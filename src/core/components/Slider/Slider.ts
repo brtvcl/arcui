630dcbd2c4c1105304ff16e0d98d05abca57d27e
Berat Vıcıl <beratvicil@gmail.com>
2023-09-26 21:24:06 +0300

    implement slider functionality

--- src/core/components/Slider/Slider.ts
+++ src/core/components/Slider/Slider.ts
@@ -1,6 +1,7 @@
-import { h, render, createElement } from "preact";
 import type { EventRecord } from "../../shared/types";
 import type { SliderState } from "./types";
+import { h, render, createElement } from "preact";
+import { useEffect, useState, useRef } from "preact/hooks";
 
 export class Slider {
 	constructor(props: Partial<SliderState>, element: HTMLDivElement) {
@@ -41,7 +42,77 @@ export class Slider {
 		const instance = this;
 
 		function createComponent() {
-			return h("div", { class: "arc-slider" }, [h("div", { class: "handle" })]);
+
+			const [isDragging, setIsDragging] = useState(false);
+			const [value, setValue] = useState(0);
+
+			//Reference to slider container
+			const containerRef = useRef(null);
+
+			//Function to move value and calculcate percentage based on mouse position
+			function moveValue(mousex: number) {
+				const container = <HTMLDivElement><unknown>containerRef.current;
+
+				//Get dimensions of slider
+				let sliderRect = container.getBoundingClientRect();
+				const width = sliderRect.width;
+				const left = sliderRect.left;
+				
+				let x = mousex - left; //x p osition within the container.
+				let percentage = x*100/width; //0-100 value of slider
+				percentage = Math.min(Math.max(percentage, 0), 100); //Clamp percentage between 0-100
+				setValue(percentage); //set value
+				
+				//dispatch event
+				// props.onChange && props.onChange(percentage);
+				
+			};
+
+			//Drag end handler function
+			function handleDragEnd() {
+				setIsDragging(false);
+			}
+
+			//Drag move handler function
+			function handleDragMove(e: MouseEvent) {
+				//If mouse is down move slider
+				if (isDragging) {
+					console.log(e.clientX);
+					e.preventDefault();
+					moveValue(e.clientX);
+				}
+			};
+
+			useEffect(() => {
+
+				//End dragging when mouse is up
+				window.addEventListener("mouseup", handleDragEnd);
+
+				//Drag slider when mouse is down
+				window.addEventListener("mousemove", handleDragMove);
+
+				return () => {
+					//Cleanup global event listeners on component unmount
+					window.removeEventListener("mouseup", handleDragEnd);
+					window.removeEventListener("mousemove", handleDragMove);
+				}
+
+			}, [isDragging])
+
+			function handleDragStart(e: MouseEvent) {
+				//Move value once for when just clicking and not dragging
+				moveValue(e.clientX);
+
+				//Set dragging true until mouse is up
+				setIsDragging(true);
+			}
+
+			return h("div", { class: "arc-slider-container", onMouseDown:handleDragStart, ref: containerRef }, [
+				h("div", { class: "arc-slider-bar" }, [
+					h("div", { class: "arc-slider-fill", style:{ width: `${value}%` }}),
+					h("div", { class: "arc-slider-handle", style:{ left: `${value}%`  }}),
+				])
+			]);
 		}
 
 		let preactEl = createElement(createComponent, state);
