e0a2f0770cc38e42e30b7045fe77dc1192a056e1
Berat Vıcıl <beratvicil@gmail.com>
2023-11-08 22:02:02 +0300

    refactor doc tabs to snippet component

--- docs/pages/button.js
+++ docs/pages/button.js
@@ -20,25 +20,23 @@ new Button({
     width: '200px'
 }, document.getElementById('primary-button'));
 
-const primaryButtonJsCode = `
-    import { Button } from '@brtvcl/arcui';
-    import '@brtvcl/arcui/dist/bundle.css';
+const primaryButtonJsCode = `import { Button } from '@brtvcl/arcui';
+import '@brtvcl/arcui/dist/bundle.css';
 
-    new Button({
-        variant: 'primary',
-        text: 'Primary',
-        width: '200px'
-    }, document.getElementById('primary-button'));`;
+new Button({
+    variant: 'primary',
+    text: 'Primary',
+    width: '200px'
+}, document.getElementById('primary-button'));`;
 
 document.getElementById('primary-button-js-code').innerHTML = highlightJsCode(primaryButtonJsCode, "javascript");
 
-const primaryButtonReactCode = `
-    import { Button } from '@brtvcl/arcui';
-    import '@brtvcl/arcui/dist/bundle.css';
+const primaryButtonReactCode = `import { Button } from '@brtvcl/arcui';
+import '@brtvcl/arcui/dist/bundle.css';
 
-    <Button variant="primary" width="200px">
-        Primary
-    <Button>`;
+<Button variant="primary" width="200px">
+    Primary
+<Button>`;
 
 document.getElementById('primary-button-react-code').innerHTML = highlightJsCode(primaryButtonReactCode, "jsx");
 
@@ -49,25 +47,16 @@ new Button({
     width: '200px',
 }, document.getElementById('secondary-button'));
 
-const secondaryButtonJsCode = `
-    import { Button } from '@brtvcl/arcui';
-    import '@brtvcl/arcui/dist/bundle.css';
+const secondaryButtonJsCode = `import { Button } from '@brtvcl/arcui';
+import '@brtvcl/arcui/dist/bundle.css';
 
-    new Button({
-        variant: 'secondary',
-        text: 'Secondary',
-        width: '200px'
-    }, document.getElementById('secondary-button'));`;
+new Button({
+    variant: 'secondary',
+    text: 'Secondary',
+    width: '200px'
+}, document.getElementById('secondary-button'));`;
 
-new Tabs({
-    items: [
-        {
-            key: "vanilla",
-            label: `<img width="24" src="/img/js.svg"/>`,
-            children: highlightJsCode(secondaryButtonJsCode, "javascript")
-        },
-    ]
-}, document.getElementById("secondary-button-code"));
+document.getElementById("secondary-button-js-code").innerHTML = highlightJsCode(secondaryButtonJsCode, "javascript");
 
 // Outline Button
 new Button({
@@ -76,25 +65,16 @@ new Button({
     width: '200px',
 }, document.getElementById('outline-button'));
 
-const outlineButtonJsCode = `
-    import { Button } from '@brtvcl/arcui';
-    import '@brtvcl/arcui/dist/bundle.css';
+const outlineButtonJsCode = `import { Button } from '@brtvcl/arcui';
+import '@brtvcl/arcui/dist/bundle.css';
 
-    new Button({
-        variant: 'outline',
-        text: 'Outline',
-        width: '200px'
-    }, document.getElementById('outline-button'));`;
+new Button({
+    variant: 'outline',
+    text: 'Outline',
+    width: '200px'
+}, document.getElementById('outline-button'));`;
 
-new Tabs({
-    items: [
-        {
-            key: "vanilla",
-            label: `<img width="24" src="/img/js.svg"/>`,
-            children: highlightJsCode(outlineButtonJsCode, "javascript")
-        },
-    ]
-}, document.getElementById("outline-button-code"));
+document.getElementById("outline-button-js-code").innerHTML = highlightJsCode(outlineButtonJsCode, "javascript");
 
 // Ghost Button
 new Button({
@@ -103,25 +83,16 @@ new Button({
     width: '200px',
 }, document.getElementById('ghost-button'));
 
-const ghostButtonJsCode = `
-    import { Button } from '@brtvcl/arcui';
-    import '@brtvcl/arcui/dist/bundle.css';
+const ghostButtonJsCode = `import { Button } from '@brtvcl/arcui';
+import '@brtvcl/arcui/dist/bundle.css';
 
-    new Button({
-        variant: 'ghost',
-        text: 'Ghost',
-        width: '200px'
-    }, document.getElementById('ghost-button'));`;
+new Button({
+    variant: 'ghost',
+    text: 'Ghost',
+    width: '200px'
+}, document.getElementById('ghost-button'));`;
 
-new Tabs({
-    items: [
-        {
-            key: "vanilla",
-            label: `<img width="24" src="/img/js.svg"/>`,
-            children: highlightJsCode(ghostButtonJsCode, "javascript")
-        },
-    ]
-}, document.getElementById("ghost-button-code"));
+document.getElementById("ghost-button-js-code").innerHTML = highlightJsCode(ghostButtonJsCode, "javascript");
 
 // Disabled Button
 new Button({
@@ -131,26 +102,19 @@ new Button({
     disabled: true
 }, document.getElementById('disabled-button'));
 
-const disabledButtonJsCode = `
-    import { Button } from '@brtvcl/arcui';
-    import '@brtvcl/arcui/dist/bundle.css';
+const disabledButtonJsCode = `import { Button } from '@brtvcl/arcui';
+import '@brtvcl/arcui/dist/bundle.css';
 
-    new Button({
-        variant: 'secondary',
-        text: 'Disabled',
-        width: '200px'
-        disabled: true,
-    }, document.getElementById('disabled-button'));`;
+new Button({
+    variant: 'secondary',
+    text: 'Disabled',
+    width: '200px'
+    disabled: true,
+}, document.getElementById('disabled-button'));`;
 
-new Tabs({
-    items: [
-        {
-            key: "vanilla",
-            label: `<img width="24" src="/img/js.svg"/>`,
-            children: highlightJsCode(disabledButtonJsCode, "javascript")
-        },
-    ]
-}, document.getElementById("disabled-button-code"));
+document.getElementById("disabled-button-js-code").innerHTML = highlightJsCode(disabledButtonJsCode, "javascript");
+
+
 
 // Loading Button
 new Button({
@@ -160,26 +124,17 @@ new Button({
     loading: true,
 }, document.getElementById('loading-button'));
 
-const loadingButtonJsCode = `
-    import { Button } from '@brtvcl/arcui';
-    import '@brtvcl/arcui/dist/bundle.css';
+const loadingButtonJsCode = `import { Button } from '@brtvcl/arcui';
+import '@brtvcl/arcui/dist/bundle.css';
 
-    new Button({
-        variant: 'secondary',
-        text: 'Loading',
-        width: '200px'
-        loading: true,
-    }, document.getElementById('loading-button'));`;
+new Button({
+    variant: 'secondary',
+    text: 'Loading',
+    width: '200px'
+    loading: true,
+}, document.getElementById('loading-button'));`;
 
-new Tabs({
-    items: [
-        {
-            key: "vanilla",
-            label: `<img width="24" src="/img/js.svg"/>`,
-            children: highlightJsCode(loadingButtonJsCode, "javascript")
-        },
-    ]
-}, document.getElementById("loading-button-code"));
+document.getElementById("loading-button-js-code").innerHTML = highlightJsCode(loadingButtonJsCode, "javascript");
 
 // Small Button
 new Button({
@@ -189,26 +144,17 @@ new Button({
     size: 'small'
 }, document.getElementById('small-button'));
 
-const smallButtonJsCode = `
-    import { Button } from '@brtvcl/arcui';
-    import '@brtvcl/arcui/dist/bundle.css';
+const smallButtonJsCode = `import { Button } from '@brtvcl/arcui';
+import '@brtvcl/arcui/dist/bundle.css';
 
-    new Button({
-        variant: 'secondary',
-        text: 'Small',
-        width: '150px',
-        size: 'small'
-    }, document.getElementById('small-button'));`;
+new Button({
+    variant: 'secondary',
+    text: 'Small',
+    width: '150px',
+    size: 'small'
+}, document.getElementById('small-button'));`;
 
-new Tabs({
-    items: [
-        {
-            key: "vanilla",
-            label: `<img width="24" src="/img/js.svg"/>`,
-            children: highlightJsCode(smallButtonJsCode, "javascript")
-        },
-    ]
-}, document.getElementById("small-button-code"));
+document.getElementById("small-button-js-code").innerHTML = highlightJsCode(smallButtonJsCode, "javascript");
 
 // Large Button
 new Button({
@@ -218,26 +164,17 @@ new Button({
     size: 'large',
 }, document.getElementById('large-button'));
 
-const largeButtonJsCode = `
-    import { Button } from '@brtvcl/arcui';
-    import '@brtvcl/arcui/dist/bundle.css';
+const largeButtonJsCode = `import { Button } from '@brtvcl/arcui';
+import '@brtvcl/arcui/dist/bundle.css';
 
-    new Button({
-        variant: 'secondary',
-        text: 'Large',
-        width: '150px',
-        size: 'large'
-    }, document.getElementById('large-button'));`;
+new Button({
+    variant: 'secondary',
+    text: 'Large',
+    width: '150px',
+    size: 'large'
+}, document.getElementById('large-button'));`;
 
-new Tabs({
-    items: [
-        {
-            key: "vanilla",
-            label: `<img width="24" src="/img/js.svg"/>`,
-            children: highlightJsCode(largeButtonJsCode, "javascript")
-        },
-    ]
-}, document.getElementById("large-button-code"));
+document.getElementById("large-button-js-code").innerHTML = highlightJsCode(largeButtonJsCode, "javascript");
 
 // Icon Button
 new Button({
@@ -247,26 +184,18 @@ new Button({
     icon: '🚀'
 }, document.getElementById('icon-button'));
 
-const iconButtonJsCode = `
-    import { Button } from '@brtvcl/arcui';
-    import '@brtvcl/arcui/dist/bundle.css';
+const iconButtonJsCode = 
+`import { Button } from '@brtvcl/arcui';
+import '@brtvcl/arcui/dist/bundle.css';
 
-    new Button({
-        variant: 'outline',
-        text: 'Icon',
-        width: '150px',
-        icon: '🚀'
-    }, document.getElementById('icon-button'));`;
+new Button({
+    variant: 'outline',
+    text: 'Icon',
+    width: '150px',
+    icon: '🚀'
+}, document.getElementById('icon-button'));`;
 
-new Tabs({
-    items: [
-        {
-            key: "vanilla",
-            label: `<img width="24" src="/img/js.svg"/>`,
-            children: highlightJsCode(iconButtonJsCode, "javascript")
-        },
-    ]
-}, document.getElementById("icon-button-code"));
+document.getElementById("icon-button-js-code").innerHTML = highlightJsCode(iconButtonJsCode, "javascript");
 
 // Square Button
 new Button({
@@ -277,121 +206,51 @@ new Button({
     square: true,
 }, document.getElementById('square-button'));
 
-const squareButtonJsCode = `
-    new Button({
-        variant: 'outline',
-        text: 'Icon', // Won't render because square is true
-        width: '150px',
-        icon: '🗑️',
-        square: true,
-    }, document.getElementById('square-button'));`;
+const squareButtonJsCode = 
+`new Button({
+    variant: 'outline',
+    text: 'Icon', // Won't render because square is true
+    width: '150px',
+    icon: '🗑️',
+    square: true,
+}, document.getElementById('square-button'));`;
 
-new Tabs({
-    items: [
-        {
-            key: "vanilla",
-            label: `<img width="24" src="/img/js.svg"/>`,
-            children: highlightJsCode(squareButtonJsCode, "javascript")
-        },
-    ]
-}, document.getElementById("square-button-code"));
+document.getElementById("square-button-js-code").innerHTML = highlightJsCode(squareButtonJsCode, "javascript");
+
+
 
 
 // Attach Event
 const attachEventJsCode = `
-    const btn = new Button({
-        variant: 'primary',
-        text: 'Primary',
-        width: '200px'
-    }, document.getElementById('primary-button'));
-    
-    btn.on("click", () => {
-        console.log("clicked");
-    });
-`;
+const btn = new Button({
+    variant: 'primary',
+    text: 'Primary',
+    width: '200px'
+}, document.getElementById('primary-button'));
+
+btn.on("click", () => {
+    console.log("clicked");
+});`;
 
 // Detach Event
 const detachEventJsCode = `
-    const btn = new Button({
-        variant: 'primary',
-        text: 'Primary',
-        width: '200px'
-    }, document.getElementById('primary-button'));
-    
-    // Attach click handler
-    const id = btn.on("click", () => {
-        console.log("clicked");
-    });
+const btn = new Button({
+    variant: 'primary',
+    text: 'Primary',
+    width: '200px'
+}, document.getElementById('primary-button'));
 
-    // Remove click handler after 5 seconds
-    setTimeout(() => {
-        btn.off(id);
-    }, 5000)
+// Attach click handler
+const id = btn.on("click", () => {
+    console.log("clicked");
+});
+
+// Remove click handler after 5 seconds
+setTimeout(() => {
+    btn.off(id);
+}, 5000)
 `;
 
 // JS Events Code
-new Tabs({
-    items: [
-        {
-            label: `<img width="24" src="/img/js.svg"/>`,
-            children: `<p>
-            The <span class="code">.on()</span> method attaches event handlers to the Arc UI components and 
-            returns an event listener <span class="code">id</span> that you can use to remove the event listener with <span class="code">.off()</span> method.
-                </p>
-                <p>
-                    You can find the event types below.
-                </p>
-                <h3>Example</h3>
-                <div class="row">
-                    <div class="col">
-                        <p>Attaching event listeners</p>
-                        <div>
-                            <td>
-                                <div id="attach-event-code">
-                                    ${highlightJsCode(attachEventJsCode, "javascript")}
-                                </div>
-                            </td>
-                        </div>
-                    </div>
-                    <div class="col">
-                        <p>Removing event listeners</p>
-                        <div>
-                            <td>
-                                <div id="detach-event-code">
-                                    ${highlightJsCode(detachEventJsCode, "javascript")}
-                                </div>
-                            </td>
-                        </div>
-                    </div>
-                </div>
-                
-                <div class="mb-4 mt-4">
-                    <table class="table">
-                        <thead>
-                            <th>Event</th>
-                            <th>Type</th>
-                            <th>Description</th>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td>
-                                    <span class="monospace">
-                                        click
-                                    </span>
-                                </td>
-                                <td>
-                                    <span class="code">
-                                        () => void
-                                    </span>
-                                    
-                                </td>
-                                <td>Fired when button is clicked</td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>`,
-        key: "vanilla"
-        }
-    ]
-}, document.getElementById("events-section"));
-
+document.getElementById("attach-event-js-code").innerHTML = highlightJsCode(attachEventJsCode, "javascript");
+document.getElementById("detach-event-js-code").innerHTML = highlightJsCode(detachEventJsCode, "javascript");
