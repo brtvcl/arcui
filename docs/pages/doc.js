7e5120aaa85e4636a4296564ad413eb9b0e02940
Berat Vıcıl <beratvicil@gmail.com>
2023-11-09 21:49:56 +0300

    add navbar to docs

--- docs/pages/doc.js
+++ docs/pages/doc.js
@@ -1,5 +1,5 @@
 // Add copy button to snippets
-import { Button } from "@brtvcl/arcui";
+import { Button, Input } from "@brtvcl/arcui";
 
 function fallbackCopyTextToClipboard(text) {
     let textArea = document.createElement("textarea");
@@ -76,3 +76,14 @@ snippets.forEach((snippet) => {
     
     
 });
+
+
+// Search bar
+new Input({
+    size: "small",
+    icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ACAFBA" class="w-6 h-6">
+    <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
+  </svg>
+  `,
+  width: "180px",
+}, document.getElementById("searchbar"))
